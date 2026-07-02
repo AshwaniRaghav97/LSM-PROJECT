dd63eea8ff551031ac69e51a94370ba7629021de
Ashwani Raghav <ashwani.raghav_cs23@gla.ac.in>
2026-07-02 09:55:23 +0530

    Implemented user login with JWT authentication

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,9 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 
+import jwt from "jsonwebtoken";
+import generateToken from "../utils/generateToken.js";
+
 export const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -45,5 +48,64 @@ export const registerUser = async (req, res) => {
       success: false,
       message: error.message,
     });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Email and Password are required",
+      });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid Credentials",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid Credentials",
+      });
+    }
+
+    const token = generateToken(user._id);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Login Successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+
   }
 };
