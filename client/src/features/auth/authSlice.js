d64a4f1809c90eef2ca317894143a2b5cfffcc45
Ashwani Raghav <ashwani.raghav_cs23@gla.ac.in>
2026-07-22 16:12:52 +0530

    some changes

--- client/src/features/auth/authSlice.js
+++ client/src/features/auth/authSlice.js
@@ -1,9 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  user: null,
-  token: null,
-  isAuthenticated: false,
+  user: JSON.parse(localStorage.getItem("user")) || null,
+  token: localStorage.getItem("token") || null,
+  isAuthenticated: !!localStorage.getItem("token"),
 };
 
 const authSlice = createSlice({
@@ -14,12 +14,18 @@ const authSlice = createSlice({
       state.user = action.payload.user;
       state.token = action.payload.token;
       state.isAuthenticated = true;
+
+      localStorage.setItem("token", action.payload.token);
+      localStorage.setItem("user", JSON.stringify(action.payload.user));
     },
 
     logout: (state) => {
       state.user = null;
       state.token = null;
       state.isAuthenticated = false;
+
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
     },
   },
 });
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -90,9 +90,11 @@ export const loginUser = async (req, res) => {
     const token = generateToken(user._id);
 
     res.cookie("token", token, {
-      httpOnly: true,
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+});
 
     res.status(200).json({
       success: true,
@@ -128,9 +130,11 @@ export const getProfile = async (req, res) => {
 export const logoutUser = (req, res) => {
 
     res.cookie("token", "", {
-        httpOnly: true,
-        expires: new Date(0),
-    });
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  expires: new Date(0),
+});
 
     res.status(200).json({
         success: true,
