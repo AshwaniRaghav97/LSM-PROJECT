6b40dccfb5035aef2999b7762f9e644b0f42c2e8
Ashwani Raghav <ashwani.raghav_cs23@gla.ac.in>
2026-07-22 16:50:57 +0530

    commited

--- server/middleware/uploadVideo.js
+++ server/middleware/uploadVideo.js
@@ -5,13 +5,16 @@ const storage = multer.memoryStorage();
 const upload = multer({
   storage,
   limits: {
-    fileSize: 100 * 1024 * 1024, // 100 MB
+    fileSize: 100 * 1024 * 1024,
   },
   fileFilter: (req, file, cb) => {
-    if (file.mimetype.startsWith("video/")) {
+    if (
+      file.mimetype.startsWith("image/") ||
+      file.mimetype.startsWith("video/")
+    ) {
       cb(null, true);
     } else {
-      cb(new Error("Only video files are allowed"));
+      cb(new Error("Only image and video files are allowed"));
     }
   },
 });
